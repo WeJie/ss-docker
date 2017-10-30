150bba778bdc4dc98fdc680ef1caca2ae6105b70
weijie <wejie00@foxmail.com>
2017-10-30 22:58:16 +0800

    Fix autoban command

--- ss.sh
+++ ss.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 ssserver -c /etc/shadowsocks.json -d start
-/home/ubuntu/autoban.py < /var/log/shadowsocks.log
+python /home/ubuntu/autoban.py < /var/log/shadowsocks.log
 nohup tail -F /var/log/shadowsocks.log | python /home/ubunntu/autoban.py >log 2>log &
 
